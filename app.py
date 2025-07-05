2e46996c04f771d328f560b46613801a3377bd4b
RaedAffes <raedaffes@gmail.com>
2025-07-05 22:50:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import easyocr as ocr  #OCR
 import streamlit as st  #Web App
 from PIL import Image #Image Processing
 import numpy as np #Image Processing 
-
+ 
 #title
 st.title("Easy OCR(Optical Character Recognition.) - Extract Text from Images")
 
